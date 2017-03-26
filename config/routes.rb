a0f80958cc4ccfb43b39e947f08075e78aade63a
Melissa Rodriguez <olenellina@gmail.com>
2017-03-25 19:47:16 -0700

    basic welcome page for site

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'welcome/index'=> 'welcome#index'
 
   get 'vitamins/new'
 
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get 'vitamins/index'
 
   get 'vitamins/show'
+
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
